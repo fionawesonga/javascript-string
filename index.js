f55ea10cdd0eefad743f500f8c85bff75eb24507
Fiona Wesonga <fionahwesonga@gmail.com>
2025-03-27 08:06:21 +0300

    changed name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Extract the last four characters from the string below;"extravaganza"
-let x = "extravaganza";
-let lastFour = x.slice(-4);
-console.log(lastFour);
+// let x = "extravaganza";
+// let lastFour = x.slice(-4);
+// console.log(lastFour);
 
 // Insert the following string at the fourth index of the below variable:"eat"const food = "The quick fox jumped over the lazy dog"
 let food = "The quick fox jumped over the lazy dog";
@@ -38,8 +38,81 @@ console.log(lowerCaseWord1);
 console.log(lowerCaseWord2); 
 
 let titleCaseWord = "A wonderful world"
-    .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(" ");
+   titleCaseWord.split(" ")
+               .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+               .join(" ");
 console.log(titleCaseWord); 
 
+let z="extremely";
+let firstfour=z.slice(4);
+console.log(firstfour);
+
+let x="extravaganza";
+let lastFour = x.slice(2,5);
+console.log(lastFour);
+
+let m="I am a girl";
+let f=m.toLowerCase();
+acount=f.split("a").length -1;
+
+// Question 1
+let arr1 = [3, 7, 34, 90, 12]
+let arr2 = [true, "green", "where", 12, 56]
+
+let lastValue = arr1.find(item => item === 12);
+console.log({lastValue});
+
+lastValue = arr2.find(item => item === 56);
+console.log({ lastValue });
+
+// Question 2
+let myPets = ["Cow", "Bird", "Snake", "Dog"];
+let pets = myPets.map(item => item).join(" ");
+console.log(typeof pets);
+console.log({pets});
+
+// Question 3
+var arr3 = [-5, 9, 5, 3, 2, -3, 6, 8, 4, 1];
+let sortedItems = arr3.sort();
+console.log({sortedItems});
+
+// Question 4
+let arr = ["boy", "man", "girl", "school", "girl", "woman"];
+
+let duplicatesItems = [];
+console.log({duplicatesItems});
+
+
+// Question 5
+let arr5 = ["the", "way", "x", 4];
+
+
+
+// Question 6
+let word = "renniw"
+let sortedString = word.split();
+console.log({sortedString});
+let newWord = sortedString.sort();
+console.log({newWord})
+
+
+// Question 7
+let fruits = new Array(10);
+console.log(fruits.length);
+fruits[5]= "Tomato";
+console.log({fruits});
+
+
+// Concat
+let primeNumbers=[2,3,4,5];
+let evenNumbers=[2,4,6,8];
+let joinedArrays=primeNumbers.concat(evenNumbers);
+console.log(joinedArrays);
+
+
+
+
+
+let people=["Jane","Anne","Paul"];
+people[people.length-1];
+console.log(people);
